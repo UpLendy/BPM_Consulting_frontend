89a544b8ee9d186c2673493d94125754155630d1
Felipe Lopez <juanf.lopezs@autonoma.edu.co>
2026-01-04 13:25:34 -0500

    feat: add admin appointment management view

---
+++ app/components/admin/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Central export for admin components
+ */
+
+export { default as AdminListView } from './AdminListView';
+export { default as SearchBar } from './SearchBar';
+export { default as FilterButtons } from './FilterButtons';
+export { default as EmptyState } from './EmptyState';
