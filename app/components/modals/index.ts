17f6cf1a1a2875a4aedb14ba94654aa72677d01a
Felipe Lopez <juanf.lopezs@autonoma.edu.co>
2026-01-04 13:16:07 -0500

    feat: add appointment modals for create view and delete

---
+++ app/components/modals/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Central export for modal components
+ */
+
+export { default as BaseModal } from './BaseModal';
+export { default as ViewAppointmentModal } from './ViewAppointmentModal';
+export { default as CreateAppointmentModal } from './CreateAppointmentModal';
+export { default as ConfirmDeleteModal } from './ConfirmDeleteModal';
